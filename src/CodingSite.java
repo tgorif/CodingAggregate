b76353e03583920610bcb06e2f8fa64f4dc6be77
Kay <Kay@192.168.2.103>
2020-08-14 18:21:10 +0200

    cp

--- src/CodingSite.java
+++ src/CodingSite.java
@@ -11,59 +11,17 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-public class CodingSite {
-	String name;
-	URL loginPage;
-	URL url;
-	int numSolved;
-	int numTotal;
-	int acceptedEasy;
-	int acceptedMedium;
-	int acceptedHard;
-	String userName;
-	ArrayList<Challenge> challenges;
-	
-	public CodingSite(String name,URL url,URL loginPage) {
-		this.loginPage =loginPage;
-		this.name = name;
-		this.url = url;
-		challenges =new ArrayList<Challenge>();
-	}
-	public void extractSubmissionsFromFile() throws IOException, ParseException {
-		Object obj = new JSONParser().parse(new FileReader("H:\\workspace\\eclipse-workspace\\CodingAggregate\\src\\Leetcode.json"));
-		JSONObject jsonobj = (JSONObject)obj;
-		long numsol = (long) jsonobj.get("num_solved");
-		numSolved = (int)numsol;
-		long ae = (long) jsonobj.get("num_solved");
-		acceptedEasy=(int)ae;
-		long am = (long) jsonobj.get("num_solved");
-		acceptedMedium =(int)am;
-		long ah = (long) jsonobj.get("num_solved");
-		acceptedHard=(int)ah;
-		userName =(String)jsonobj.get("user_name");
-		long numt = (long)jsonobj.get("num_total");
-		numTotal=(int)numt;
-		JSONArray jsonarr =(JSONArray)jsonobj.get("stat_status_pairs");
-		Iterator it = jsonarr.iterator();
-		while(it.hasNext()) {
-			JSONObject o = (JSONObject) it.next();
-			JSONObject inner =(JSONObject) o.get("stat");
-			JSONObject dif =(JSONObject) o.get("difficulty");
-			boolean b=true;
-			String t = (String)o.get("status");
-			if(t==null) {
-				b=false;
-			}
-			long difficulty =(long)dif.get("level");
-			Challenge c = new Challenge(this,(String)inner.get("question__title"),b,(int)difficulty);
-			challenges.add(c);
-		}
-	}
-	public void getSolvedChallenges() {
-		for(Challenge c : challenges) {
-			if(c.solved) {
-				System.out.println(c.name + " " + c.difficulty);
-			}
-		}
-	}
+public interface CodingSite {
+	String name=null;
+	URL loginPage=null;
+	URL url=null;
+	int numSolved=0;
+	int numTotal=0;
+	int acceptedEasy=0;
+	int acceptedMedium=0;
+	int acceptedHard=0;
+	String userName=null;
+	ArrayList<Challenge> challenges=null;
+	public void extractSubmissionsFromFile() throws IOException, ParseException;
+	public void getSolvedChallenges();
 }
--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import org.json.simple.parser.ParseException;
 public class Main {
 
 	public static void main(String[] args) throws IOException, ParseException {
-		CodingSite leetcode = new CodingSite("leetcode",new URL("https://leetcode.com/api/problems/algorithms/"),new URL("https://leetcode.com/"));
+		CodingSite leetcode = new Leetcode("leetcode",new URL("https://leetcode.com/api/problems/algorithms/"),new URL("https://leetcode.com/"));
 		leetcode.extractSubmissionsFromFile();
 		leetcode.getSolvedChallenges();
 		// TODO Auto-generated method stub
